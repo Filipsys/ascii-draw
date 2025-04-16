ebb7ae848d3773d2e73aa50f97c4141ab369b65c
Filipsys <filipsysak@gmail.com>
2025-04-17 00:43:20 +0200

    Created basic outline of the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import { createSignal } from "solid-js";
+import { /* createEffect, */ createSignal } from "solid-js";
 import "./globals.css";
 
-const dimensions = {
-  width: 30,
-  height: 10
-}
 function App() {
-  const [map, setMap] = createSignal<string[][]>(Array(dimensions.height).fill(".").map(() => Array(dimensions.width).fill(".")));
-  
+  const [drawing, setDrawing] = createSignal<boolean>(false);
+  const [dimensions, setDimensions] = createSignal({
+    characterWidth: 30,
+    characterHeight: 10,
+    width: 0,
+    height: 0,
+  });
+  const [map, _setMap] = createSignal<string[][]>(
+    Array(dimensions().characterHeight)
+      .fill(".")
+      .map(() => Array(dimensions().characterWidth).fill(" ")),
+  );
 
   const renderMap = (map: string[][]) => {
     let renderedMap = "";
@@ -19,16 +25,37 @@ function App() {
     }
 
     return renderedMap;
-  }
+  };
+
+  // createEffect(() => {
+  //   const drawDiv = document.getElementById("draw-canvas");
+  //   if (!drawDiv) return;
+
+  //   setDimensions({
+  //     ...dimensions(),
+  //     width: drawDiv.clientWidth,
+  //     height: drawDiv.clientHeight,
+  //   });
+
+  //   console.log(dimensions());
+  // });
 
   return (
     <div class="bg-neutral-900 text-neutral-300 font-mono text-base h-dvh flex flex-col justify-center items-center">
       <h1 class="text-5xl text-blue-400">Vite + Solid</h1>
 
-      <div class="p-2 border-[1px] border-white">
-        <pre>
-        {renderMap(map())}
-        </pre>
+      <div
+        id="draw-canvas"
+        class="p-2 border-[1px] border-white select-none"
+        on:keydown={() => setDrawing(true)}
+        on:keyup={() => setDrawing(false)}
+        on:mouseleave={() => setDrawing(false)}
+        on:focusout={() => setDrawing(false)}
+        on:mouseover={(_event) => {
+          if (!drawing()) return;
+        }}
+      >
+        <pre>{renderMap(map())}</pre>
       </div>
     </div>
   );
