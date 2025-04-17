01926ff7547d6c9be70b3e2a617c687228586ce0
Filipsys <filipsysak@gmail.com>
2025-04-17 19:40:25 +0200

    Drawing using the dot symbol complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
-import { /* createEffect, */ createSignal } from "solid-js";
+import { createSignal, onMount, createEffect } from "solid-js";
 import "./globals.css";
 
 function App() {
   const [drawing, setDrawing] = createSignal<boolean>(false);
-  const [dimensions, _setDimensions] = createSignal({
-    characterWidth: 30,
-    characterHeight: 10,
+  const [dimensions, setDimensions] = createSignal({
+    characterWidth: 60,
+    characterHeight: 30,
     width: 0,
     height: 0,
   });
-  const [map, _setMap] = createSignal<string[][]>(
-    Array(dimensions().characterHeight)
-      .fill(".")
-      .map(() => Array(dimensions().characterWidth).fill(" ")),
-  );
+  const [map, setMap] = createSignal<string[][]>([]);
+
+  onMount(() => {
+    setMap(
+      Array(dimensions().characterHeight)
+        .fill(null)
+        .map(() => Array(dimensions().characterWidth).fill(" ")),
+    );
+  });
 
   const renderMap = (map: string[][]) => {
     let renderedMap = "";
@@ -27,32 +31,49 @@ function App() {
     return renderedMap;
   };
 
-  // createEffect(() => {
-  //   const drawDiv = document.getElementById("draw-canvas");
-  //   if (!drawDiv) return;
+  const getLocation = (currentX: number, currentY: number) => {
+    const { width, height, characterWidth, characterHeight } = dimensions();
 
-  //   setDimensions({
-  //     ...dimensions(),
-  //     width: drawDiv.clientWidth,
-  //     height: drawDiv.clientHeight,
-  //   });
+    return {
+      x: Math.floor(currentX / (width / characterWidth)),
+      y: Math.floor(currentY / (height / characterHeight)),
+    };
+  };
 
-  //   console.log(dimensions());
-  // });
+  createEffect(() => {
+    onMount(() => {
+      const drawDiv = document.getElementById("draw-canvas");
+      if (!drawDiv) return;
+
+      setDimensions({
+        ...dimensions(),
+        width: drawDiv.clientWidth,
+        height: drawDiv.clientHeight,
+      });
+    });
+  });
 
   return (
     <div class="bg-neutral-900 text-neutral-300 font-mono text-base h-dvh flex flex-col justify-center items-center">
-      <h1 class="text-5xl text-blue-400">Vite + Solid</h1>
-
       <div
         id="draw-canvas"
-        class="p-2 border-[1px] border-white select-none"
-        on:keydown={() => setDrawing(true)}
-        on:keyup={() => setDrawing(false)}
-        on:mouseleave={() => setDrawing(false)}
-        on:focusout={() => setDrawing(false)}
-        on:mouseover={(_event) => {
+        class="p-2 border-[1px] border-white/20 select-none"
+        onMouseDown={() => setDrawing(true)}
+        onMouseUp={() => setDrawing(false)}
+        onMouseLeave={() => setDrawing(false)}
+        onBlur={() => setDrawing(false)}
+        onMouseOut={() => setDrawing(false)}
+        onFocusOut={() => setDrawing(false)}
+        onMouseMove={(event) => {
           if (!drawing()) return;
+
+          const { x, y } = getLocation(event.offsetX, event.offsetY);
+          setMap((previous) => {
+            const newGrid = previous.map((row) => [...row]);
+            newGrid[y][x] = ".";
+
+            return newGrid;
+          });
         }}
       >
         <pre>{renderMap(map())}</pre>
